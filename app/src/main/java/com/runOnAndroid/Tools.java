48f41239bb8e15591d74b389cb4c03d05dfee9e7
BigNoseDewdrop <woyaogtmdttkp@gmail.com>
2021-10-24 17:01:35 +0800

    try to expand the layout to the camera area (for notch)

--- app/src/main/java/com/runOnAndroid/Tools.java
+++ app/src/main/java/com/runOnAndroid/Tools.java
@@ -1,13 +1,14 @@
 package com.runOnAndroid;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Looper;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import java.util.Objects;
 
 public class Tools {
@@ -50,13 +51,21 @@ public class Tools {
         //hide ActionBar
         Objects.requireNonNull(activity.getSupportActionBar()).hide();
 
+        //try to expand the layout to the camera area (for notch)
+        if (activity.getResources().getConfiguration().orientation ==
+                Configuration.ORIENTATION_LANDSCAPE && Build.VERSION.SDK_INT >= 28){
+            WindowManager.LayoutParams lp = activity.getWindow().getAttributes();
+            lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+        }
+
         //hide virtual keys
         activity.getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         |View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         |View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         |View.SYSTEM_UI_FLAG_IMMERSIVE
-                        |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+                        |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        |View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
         //hide top status bar
         activity.getWindow().setStatusBarColor(Color.TRANSPARENT);
